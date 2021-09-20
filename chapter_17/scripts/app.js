fc4e496b063016a76323d297eb8f0fc1f4e2ba36
Hyejin Lee <hichjay@gmail.com>
2021-09-21 00:16:21 +0900

    Section 17: Project - Real-time Chatroom
    - Firestore (add data)
    - Async & await (promise)
    - use class (Chatroom, UI)

--- chapter_17/scripts/app.js
+++ chapter_17/scripts/app.js
@@ -9,6 +9,7 @@ const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
 const newNameForm = document.querySelector(".new-name");
 const updateMssg = document.querySelector(".update-mssg");
+const rooms = document.querySelector(".chat-rooms");
 
 // add a new chat
 newChatForm.addEventListener("submit", (e) => {
@@ -36,12 +37,21 @@ newNameForm.addEventListener("submit", (e) => {
   updateMssg.innerText = `Your name was updated to ${newName}`;
 });
 
+// update the chat room
+rooms.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute("id"));
+    chatroom.getChats((chat) => chatUI.render(chat));
+  }
+});
+
 // check local storage for a name
-const username = localStorage.username ? localStorage.username : "user";
+const username = localStorage.username ? localStorage.username : "anonymous";
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "Nicole");
+const chatroom = new Chatroom("general", username);
 
 // get chats and render
 chatroom.getChats((data) => chatUI.render(data));
