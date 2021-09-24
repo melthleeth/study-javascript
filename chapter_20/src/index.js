6f21e35d378914383225b0c7ccdc5dc4fc99e4c5
Hyejin Lee <hichjay@gmail.com>
2021-09-24 23:45:39 +0900

    Section 20: Project - UI Library (ing)

---
+++ chapter_20/src/index.js
@@ -0,0 +1,6 @@
+import "./test.css";
+import Tooltip from "./ui/tooltip";
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+tooltip.init();
---
+++ chapter_20/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
